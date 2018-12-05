39ea0352208ea669041da50099f06ff0ce051cdc
jsliang <1741020489@qq.com>
2018-12-05 21:58:30 +0800

    Node 学习

---
+++ Node/05_fs.js
@@ -0,0 +1,52 @@
+/**
+ * 1. fs.stat 检测是文件还是目录
+ * 2. fs.mkdir 创建目录
+ * 3. fs.writeFile 创建写入文件
+ * 4. fs.appendFile 追加文件
+ * 5. fs.readFile 读取文件
+ * 6. fs.readdir 读取目录
+ * 7. fs.rename 重命名
+ * 8. fs.rmdir 删除目录
+ * 9. fs.unlink 删除文件
+ */
+
+//  1. fs.stat
+let fs = require('fs');
+fs.stat('index.js', (error, stats) => {
+  if(error) {
+    console.log(error);
+    return false;
+  } else {
+    console.log(stats);
+    /**
+     * Console：
+     * Stats {
+     *  dev: 886875,
+     *  mode: 33206,
+     *  nlink: 1,
+     *  uid: 0,
+     *  gid: 0,
+     *  rdev: 0,
+     *  blksize: undefined,
+     *  ino: 844424931461390,
+     *  size: 284,
+     *  blocks: undefined,
+     *  atimeMs: 1542847157494,
+     *  mtimeMs: 1543887546361.2158,
+     *  ctimeMs: 1543887546361.2158,
+     *  birthtimeMs: 1542847157493.663,
+     *  atime: 2018-11-22T00:39:17.494Z,
+     *  mtime: 2018-12-04T01:39:06.361Z,
+     *  ctime: 2018-12-04T01:39:06.361Z,
+     *  birthtime: 2018-11-22T00:39:17.494Z }
+     */
+
+    console.log(`文件：${stats.isFile()}`); 
+    // Console：文件：true
+    
+    console.log(`目录：${stats.isDirectory()}`); 
+    // Console：目录：false
+
+    return false;
+  }
+})
