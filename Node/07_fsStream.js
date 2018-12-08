067530d0476ec816dcadd504c0ae5d3e7e782c1c
jsliang <1741020489@qq.com>
2018-12-08 12:59:27 +0800

    Node 学习

--- Node/07_fsStream.js
+++ Node/07_fsStream.js
@@ -1,22 +1,13 @@
-const fs = require('fs');
+let fs = require('fs');
+let data = 'console.log("Hello World! 我要存入数据！")';
 
-let fileReadStream = fs.createReadStream('index.js');
-
-let count = 0;
-
-let str = '';
-
-fileReadStream.on('data', (chunk) => {
-  console.log(`${++count} 接收到：${chunk.length}`);
-  str += chunk;
-})
-
-fileReadStream.on('end', () => {
-  console.log("——结束——");
-  console.log(count);
-  console.log(str);
-})
-
-fileReadStream.on('error', (error) => {
-  console.log(error);
+// 创建一个可以写入的流，写入到文件 index.js 中
+let writeStream = fs.createWriteStream('index.js');
+// 开始写入
+writeStream.write(data, 'utf8');
+// 写入完成
+writeStream.end();
+writeStream.on('finish', () => {
+  console.log('写入完成！');
+  // Console：写入完成
 })
