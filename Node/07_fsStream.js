2411a29c8d35b6e50038abd74e0a0fda53244065
jsliang <1741020489@qq.com>
2018-12-08 10:57:59 +0800

    Node 学习

---
+++ Node/07_fsStream.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+let fileReadStream = fs.createReadStream('index.js');
+
+let count = 0;
+
+let str = '';
+
+fileReadStream.on('data', (chunk) => {
+  console.log(`${++count} 接收到：${chunk.length}`);
+  str += chunk;
+})
+
+fileReadStream.on('end', () => {
+  console.log("——结束——");
+  console.log(count);
+  console.log(str);
+})
+
+fileReadStream.on('error', (error) => {
+  console.log(error);
+})
