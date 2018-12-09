771253a7be053cfaec376293afe671ad8dcc4c22
jsliang <1741020489@qq.com>
2018-12-09 13:03:56 +0800

    Node Web 服务器

--- Node/08_WebService.js
+++ Node/08_WebService.js
@@ -1,11 +1,38 @@
 // 引入 http 模块
-var http = require('http');
+let http = require("http");
 
-http.createServer( (req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html; charset='utf-8'"
-  });
-  res.write("Hello World!");
-  // 结束响应
-  res.end();
+// 引入 fs 模块
+let fs = require("fs");
+
+http.createServer((req, res) => {
+  // 获取响应路径
+  let pathName = req.url;
+
+  // 默认加载路径
+  if (pathName == "/") {
+    // 默认加载的首页
+    pathName = "index.html";
+  }
+
+  // 过滤 /favicon.ico 的请求
+  if (pathName != "/favicon.ico") {
+    // 获取 08_WebService 下的 index.html
+    fs.readFile("./08_WebService/" + pathName, (err, data) => {
+      if (err) {
+        // 如果不存在这个文件
+        console.log("404 Not Found!");
+        return;
+      } else {
+        // 返回这个文件
+        // 设置请求头
+        res.writeHead(200, {
+          "Content-Type": "text/html; charset='utf-8'"
+        });
+        // 读取写入文件
+        res.write(data);
+        // 结束响应
+        res.end();
+      }
+    });
+  }
 }).listen(8080);
