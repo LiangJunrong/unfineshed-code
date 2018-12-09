b57ab8c8f223cf6b99adfac3a1a15decb6779282
jsliang <1741020489@qq.com>
2018-12-09 13:28:38 +0800

    Node Web 服务器

--- Node/08_WebService.js
+++ Node/08_WebService.js
@@ -4,7 +4,11 @@ let http = require("http");
 // 引入 fs 模块
 let fs = require("fs");
 
+// 引入 path 模块
+let path = require("path");
+
 http.createServer((req, res) => {
+  
   // 获取响应路径
   let pathName = req.url;
 
@@ -14,33 +18,36 @@ http.createServer((req, res) => {
     pathName = "index.html";
   }
 
+  // 获取文件的后缀名
+  let extName = path.extname(pathName);
+
   // 过滤 /favicon.ico 的请求
   if (pathName != "/favicon.ico") {
     // 获取 08_WebService 下的 index.html
     fs.readFile("./08_WebService/" + pathName, (err, data) => {
+      // 如果不存在这个文件
       if (err) {
-        
-        // 如果不存在这个文件
-        
         console.log("404 Not Found!");
-        fs.readFile('./08_WebService/404.html', (errorNotFound, dataNotFound) => {
-          if(errorNotFound) {
-            console.log(errorNotFound);
-          } else {
-            res.writeHead(200, {
-              "Content-Type": "text/html; charset='utf-8'"
-            });
-            // 读取写入文件
-            res.write(dataNotFound);
-            // 结束响应
-            res.end();
+        fs.readFile(
+          "./08_WebService/404.html",
+          (errorNotFound, dataNotFound) => {
+            if (errorNotFound) {
+              console.log(errorNotFound);
+            } else {
+              res.writeHead(200, {
+                "Content-Type": "text/html; charset='utf-8'"
+              });
+              // 读取写入文件
+              res.write(dataNotFound);
+              // 结束响应
+              res.end();
+            }
           }
-        })
+        );
         return;
-      } else {
-
-        // 返回这个文件
-        
+      }
+      // 返回这个文件
+      else {
         // 设置请求头
         res.writeHead(200, {
           "Content-Type": "text/html; charset='utf-8'"
@@ -53,3 +60,13 @@ http.createServer((req, res) => {
     });
   }
 }).listen(8080);
+
+// 获取后缀名
+getExt = (extName) => {
+  switch(extName) {
+    case '.html': return 'text/html';
+    case '.css': return 'text/css';
+    case '.js': return 'text/js';
+    default: return 'text/html';
+  }
+}
