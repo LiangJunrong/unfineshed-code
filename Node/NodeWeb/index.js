2777224a2b1e098d63998ea6ab8ebdf6be0698ce
jsliang <1741020489@qq.com>
2018-12-17 15:31:05 +0800

    Node API

--- Node/NodeWeb/index.js
+++ Node/NodeWeb/index.js
@@ -1,41 +1,54 @@
-var http = require('http')
+// 加载 http 模块
+var http = require('http');
 
-var items = []
+// 虚拟 SQL 读取出来的数据
+var items = [];
 
+// 创建 http 服务
 http.createServer(function (req, res) {
-  // 设置cors跨域
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  res.setHeader('Content-Type', 'application/json')
+  
+  // 设置跨域的域名，* 代表允许任意域名跨域
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // 设置 header 类型
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  // 跨域允许的请求方式
+  res.setHeader('Content-Type', 'application/json');
 
+  // 判断请求
   switch (req.method) {
-    // 设置了cors跨域
-    // post请求时，浏览器会先发一次options请求，如果请求通过，则继续发送正式的post请求
+    
+    // post 请求时，浏览器会先发一次 options 请求，如果请求通过，则继续发送正式的 post 请求
     case 'OPTIONS':
-      res.statusCode = 200
-      res.end()
-      break
+      res.statusCode = 200;
+      res.end();
+      break;
+    
+      // 如果是 get 请求，则直接返回 items 数组
     case 'GET':
-      let data = JSON.stringify(items)
-      res.write(data)
-      res.end()
-      break
+      let data = JSON.stringify(items);
+      res.write(data);
+      res.end();
+      break;
+      
+    // 如果是 post 请求
     case 'POST':
-      let item = ''
+      let item = '';
+      // 读取每次发送的数据
       req.on('data', function (chunk) {
-        item += chunk
-      })
+        item += chunk;
+      });
+      // 数据发送完成
       req.on('end', function () {
         // 存入
-        item = JSON.parse(item)
-        items.push(item.item)
-        // 返回到客户端
-        let data = JSON.stringify(items)
-        res.write(data)
-        res.end()
-      })
-      break
+        item = JSON.parse(item);
+        items.push(item.item);
+        // 将数据返回到客户端
+        let data = JSON.stringify(items);
+        res.write(data);
+        res.end();
+      });
+      break;
   }
 }).listen(3000)
 
-console.log('http server is start...')
+console.log('http server is start...');
