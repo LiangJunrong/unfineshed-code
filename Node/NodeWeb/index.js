7035ef71d98af0d971aa280e84b3af387c2b0aba
jsliang <1741020489@qq.com>
2018-12-17 22:31:17 +0800

    Node 学习

--- Node/NodeWeb/index.js
+++ Node/NodeWeb/index.js
@@ -1,54 +1,51 @@
-// 加载 http 模块
-var http = require('http');
+// 引入 http 模块
+const http = require("http");
 
-// 虚拟 SQL 读取出来的数据
-var items = [];
+// 引入 url 模块
+const url = require("url");
 
-// 创建 http 服务
+// import/export
+const tools = require("jsliang-node/tools");
+
+// fs 模块
+const fs = require("fs");
+
+// 用 http 模块创建服务
+/**
+ * req 获取 url 信息 (request)
+ * res 浏览器返回响应信息 (response)
+ */
 http.createServer(function (req, res) {
-  
-  // 设置跨域的域名，* 代表允许任意域名跨域
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // 设置 header 类型
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  // 跨域允许的请求方式
-  res.setHeader('Content-Type', 'application/json');
+ 
+  // 防止 url 请求 2 次
+  if(req.url != "/favicon.ico") {
+    
+    // 判断 url
+    console.log(req.url);
 
-  // 判断请求
-  switch (req.method) {
+    // 引用工具库
+    console.log(tools.add(1, 2, 3));
     
-    // post 请求时，浏览器会先发一次 options 请求，如果请求通过，则继续发送正式的 post 请求
-    case 'OPTIONS':
-      res.statusCode = 200;
-      res.end();
-      break;
-    
-      // 如果是 get 请求，则直接返回 items 数组
-    case 'GET':
-      let data = JSON.stringify(items);
-      res.write(data);
-      res.end();
-      break;
-      
-    // 如果是 post 请求
-    case 'POST':
-      let item = '';
-      // 读取每次发送的数据
-      req.on('data', function (chunk) {
-        item += chunk;
-      });
-      // 数据发送完成
-      req.on('end', function () {
-        // 存入
-        item = JSON.parse(item);
-        items.push(item.item);
-        // 将数据返回到客户端
-        let data = JSON.stringify(items);
-        res.write(data);
-        res.end();
-      });
-      break;
+    // 引用 fs
+    fs.readFile("index.js", (err) => {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log("读取 index.js 成功！");
+      }
+    })
+
   }
-}).listen(3000)
 
-console.log('http server is start...');
+  // 设置 HTTP 头部，状态码是 200，文件类型是 html，字符集是 utf8
+  res.writeHead(200, {
+    "Content-Type": "text/html;charset=UTF-8"
+  });
+
+  // 往页面打印值
+  res.write('<h1 style="text-align:center">Hello NodeJS</h1>');
+
+  // 结束响应
+  res.end();
+
+}).listen(3000); // 监听的端口
