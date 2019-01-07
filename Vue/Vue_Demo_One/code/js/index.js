035b028b593fbca62ba9f8864cd4237f8c69cc24
jsliang <1741020489@qq.com>
2019-01-07 22:56:59 +0800

    未完成 - Vue Demo

--- Vue/Vue_Demo_One/code/js/index.js
+++ Vue/Vue_Demo_One/code/js/index.js
@@ -8,6 +8,7 @@ var app = new Vue({
       // {
       //   id: 10, // id 唯一且自增
       //   isChecked: false, // 未完成和放弃为 false，完成为 true
+      //   isEdit: false, // 是否在编辑
       //   todoTitle: "敌军 1", // todo 标题
       //   state: 0, // 0 - 未完成，1 - 完成，2 - 放弃完成
       // },
@@ -17,10 +18,11 @@ var app = new Vue({
     addTodoItem() {
       // 每次点击，往数组中添加一组数据
       this.todoInfos.push({
-        id: this.id,
-        isChecked: false,
-        todoTitle: this.todo,
-        state: 0
+        id: this.id, // id 唯一且自增
+        isChecked: false, // 未完成和放弃为 false，完成为 true
+        isEdit: false, // 是否在编辑
+        todoTitle: this.todo,  // todo 标题
+        state: 0, // 0 - 未完成，1 - 完成，2 - 放弃完成
       })
       // id 自增
       this.id++;
