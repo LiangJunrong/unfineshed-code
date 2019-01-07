4e397450fa0673f401d1116950f8e8ea56896f9b
jsliang <1741020489@qq.com>
2019-01-07 09:48:22 +0800

    Vue Demo - 功成千骨

--- Vue/Vue_Demo_One/code/js/index.js
+++ Vue/Vue_Demo_One/code/js/index.js
@@ -1,1 +1,29 @@
-// 未完待续
+var app = new Vue({
+  el: "#app",
+  data: {
+    id: 1, // 自增，确保能快速找到数据
+    todo: '', // 双向绑定输入框
+    todoInfos: [
+      // 已不需要，注释掉
+      // {
+      //   id: 10, // id 唯一且自增
+      //   isChecked: false, // 未完成和放弃为 false，完成为 true
+      //   todoTitle: "敌军 1", // todo 标题
+      //   state: 0, // 0 - 未完成，1 - 完成，2 - 放弃完成
+      // },
+    ]
+  },
+  methods: {
+    addTodoItem() {
+      // 每次点击，往数组中添加一组数据
+      this.todoInfos.push({
+        id: this.id,
+        isChecked: false,
+        todoTitle: this.todo,
+        state: 0
+      })
+      // id 自增
+      this.id++;
+    }
+  }
+})
