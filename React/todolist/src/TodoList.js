2ed91cd14f1b59fa99831cc75924eb9b385dae58
jsliang <1741020489@qq.com>
2019-03-20 21:32:31 +0800

    React Demo One - 仿简书项目

--- React/todolist/src/TodoList.js
+++ React/todolist/src/TodoList.js
@@ -1,6 +1,12 @@
 // Fragment 是一种占位符形式，类似于 Vue 的 Template
 import React, { Component, Fragment } from 'react';
 
+// 引入组件
+import TodoItem from './TodoItem';
+
+// 引用样式
+import './style.css';
+
 class TodoList extends Component {
 
   // 构造函数
@@ -11,53 +17,72 @@ class TodoList extends Component {
       inputValue: '',
       list: []
     }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
   }
 
   // 渲染页面
   render() {
-    let closeStyle = {
-      fontSize: '1.2em',
-      color: 'deepskyblue'
-    }
     return (
       <Fragment>
         <div>
+          <label htmlFor="insertArea">输入内容：</label>
           {/* 单项数据绑定 */}
           {/* 在 React 中，绑定时间的，一般为半驼峰形式 */}
           <input 
+            id="insertArea"
             type="text" 
             value={this.state.inputValue}
-            onChange={this.handleInputChange.bind(this)}
+            onChange={this.handleInputChange}
           />
-          <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+          <button onClick={this.handleBtnClick}>提交</button>
         </div>
         <ul>
-          {
-            this.state.list.map( (item, index) => {
-              return <li key={index}>
-                <span>{index} - {item}</span>
-                <span style={closeStyle} onClick={this.handleItemDelete.bind(this, index)}>×</span>
-              </li>
-            })
-          }
+          {/* 精简 JSX，将部分抽取出来 */}
+          { this.getTodoItem() }
         </ul>
       </Fragment>
     )
   }
 
+  // 获取单独项
+  getTodoItem() {
+    return this.state.list.map( (item, index) => {
+      return (
+        <TodoItem 
+          key={index}
+          item={item} 
+          index={index}
+          handleItemDelete={this.handleItemDelete}
+        />
+      )
+    })
+  }
+
   // 方法体 - 输入内容
   handleInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
-    })
+    const value = e.target.value;
+    this.setState( () => ({
+      inputValue: value
+    }))
   }
 
   // 方法体 - 点击提交
   handleBtnClick() {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
+    const list = this.state.list,
+          inputValue = this.state.inputValue;
+    this.setState( () => ({
+      list: [...list, inputValue],
       inputValue: ''
-    })
+    }))
+
+    // 或者可以这样写：
+    // this.setState( (prevState) => ({
+    //   list: [...prevState.list, prevState.inputValue],
+    //   inputValue: ''
+    // }))
+
   }
 
   // 方法体 - 删除项目
@@ -66,9 +91,9 @@ class TodoList extends Component {
     const list = [...this.state.list];
     list.splice(index, 1);
 
-    this.setState({
+    this.setState( () => ({
       list: list
-    })
+    }))
   }
 
 }
